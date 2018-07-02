ce86fbce69a2d9aa253e3c4967f4378d449c1f6d
武小奇 <1321972760@qq.com>
2018-07-02 22:53:09 +0800

    ColumnExpandableListViewAdapter

--- app/src/main/java/com/welcome/home/playandroid/adapter/ColumnExpandableListViewAdapter.kt
+++ app/src/main/java/com/welcome/home/playandroid/adapter/ColumnExpandableListViewAdapter.kt
@@ -1,9 +1,12 @@
 package com.welcome.home.playandroid.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseExpandableListAdapter
+import android.widget.TextView
+import com.welcome.home.playandroid.R
 import com.welcome.home.playandroid.bean.ColumnList
 
 class ColumnExpandableListViewAdapter(context: Context) : BaseExpandableListAdapter() {
@@ -27,45 +30,59 @@ class ColumnExpandableListViewAdapter(context: Context) : BaseExpandableListAdap
     }
 
 
-    override fun getGroup(groupPosition: Int): Int? {
-        return mColumnLists?.size
+    override fun getGroup(groupPosition: Int): ColumnList? {
+        return mColumnLists?.get(groupPosition)
     }
 
     override fun getChildrenCount(groupPosition: Int): Int {
         if (mColumnLists == null) return 0
-        return mColumnLists?.get(groupPosition)?.children?.size!!
+        return mColumnLists?.get(groupPosition)?.children?.size ?: 0
     }
 
     override fun getGroupCount(): Int {
-        return mColumnLists?.size!!
-    }
-
-    override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun hasStableIds(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getGroupView(groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getChild(groupPosition: Int, childPosition: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return mColumnLists?.size ?: 0
     }
 
     override fun getGroupId(groupPosition: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return groupPosition.toLong()
     }
 
     override fun getChildId(groupPosition: Int, childPosition: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return childPosition.toLong()
     }
 
+    override fun hasStableIds(): Boolean {
+        return false
+    }
+
+    override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean {
+        return false
+    }
+
+    override fun getChild(groupPosition: Int, childPosition: Int): Any? {
+        return mColumnLists?.get(groupPosition)?.children?.get(childPosition)
+    }
+
+    override fun getGroupView(groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View {
+        var recyclerview = convertView
+        if (recyclerview == null) {
+            recyclerview = LayoutInflater.from(mContext).inflate(R.layout.item_column, parent, false)
+        }
+        recyclerview?.findViewById<TextView>(R.id.column_tv)
+                ?.setText(getGroup(groupPosition)?.name)
+        return recyclerview!!
+    }
+
+    override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup?): View {
+        var recyclerview = convertView
+        if (recyclerview == null) {
+            recyclerview = LayoutInflater.from(mContext).inflate(R.layout.item_column_column, parent, false);
+        }
+        recyclerview?.findViewById<TextView>(R.id.column_tv)
+                ?.setText(getGroup(groupPosition)?.children?.get(childPosition)?.name)
+        recyclerview?.setOnClickListener { v ->
+            //TODO
+        }
+        return recyclerview!!
+    }
 }
